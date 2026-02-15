8bcb339599d08975d4650917dcdbe4fadee8d2b2
Mirodil Fayzullayev <odilfayz22@gmail.com>
2026-02-15 22:16:43 +0500

    .

---
+++ java/w3/first_steps/MyProgram.java
@@ -0,0 +1,18 @@
+public class MyProgram {
+
+	public static void main(String[] args){
+		System.out.println("Hello");
+	}
+
+
+}
+
+class Human{
+	static void main() {
+		System.out.println("Hello");
+	}
+}
+
+class Animal{
+
+}
